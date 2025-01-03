918545f0740d803e679471b0b56dc00b07262262
kunxin-chor <chorkunxin@yahoo.com>
2025-01-03 08:45:27 +0000

    Added conditional rendering and color button example

--- 02-components/src/App.jsx
+++ 02-components/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import apples from "./assets/apples.jpg";
+import bubbleTea from "./assets/Bubble_Tea.png"
 import "./App.css";
 
 // Create a component that display an image
 // and has a border around it
-function BorderImageFrame() {
+function BorderImageFrame(props) {
   return (
 
     /*
       <img src="apple.jpg" style="border:3px solid red;margin:10px;padding:10px"
-
     */
 
     // Inline CSS must be provided as an object
-    <img src={apples} style={{
+    <img src={props.img} style={{
       border:"3px solid red",
       margin:"10px",
       padding:"10px"
@@ -35,7 +35,6 @@ function ImageFrame() {
 // 4. can recieve parameters in the form of props
 // - props are objects and is the first argument/parameter of all component functions
 function AlertMessage(props) {
-
 
   const message = props.message;
   const color = props.bgColor;
@@ -60,6 +59,31 @@ function sayHappyNewYear() {
   return <h3>Happy New Year</h3>
 }
 
+function ShoutMessage(props) {
+  // props should contain a `msg` key which is to store the message
+  const msg = props.msg;
+  return <div>{msg.toUpperCase()}</div>
+}
+
+// expects:
+//  - props.number1 -- the first number
+//  - props.number2 -- the second number
+function SumOfTwo(props) {
+  const {number1, number2} = props;   
+  // const number1 = props.number1;
+  // const number2 = props.number2;
+  const number = number1 + number2;
+  return <div>
+    Sum of number = {number}
+  </div>
+}
+
+function SumOfTwoV2(props) {
+  return <div>
+    Sum of number = {props.number1 + props.number2}
+  </div>
+}
+
 export default function App() {
   return (
     <>
@@ -68,12 +92,15 @@ export default function App() {
     {sayHappyNewYear()}
     {/* When we want to call a Component function
     we can treat it as if it is a JSX element */}
+    <ShoutMessage msg="Hello World"/>
     <AlertMessage message="Login Failed" bgColor="pink"/>
     <AlertMessage message="Login Successful" bgColor="green"/>
+    <SumOfTwo number1={2} number2={3}/>
+    <SumOfTwo number1={10}  number2={5}/>
     <ImageFrame/>
     <ImageFrame/>
-    <BorderImageFrame/>
-    <BorderImageFrame/>
+    <BorderImageFrame img={apples}/>
+    <BorderImageFrame img={bubbleTea}/>
     </>
   )
 }
--- 03-events-and-state/src/ColoredBox.jsx
+++ 03-events-and-state/src/ColoredBox.jsx
@@ -1,16 +1,42 @@
 import { useState } from 'react'
-
+import apples from "./assets/apples.jpg";
+import watermelon from "./assets/Watermelon.webp";
 export default function ColoredBox() {
+
+    const [color, setColor] = useState("green");
+
+    const redButtonClick = () => {
+        setColor("red");
+    }
+
+    const drawFruit = () => {
+        if (color == "red") {
+            return apples;
+        } else if (color == "green") {
+            return watermelon;
+        }
+        return null;
+    }
+
     return (
         <>
+
+            {/* <img src={drawFruit()}/> */}
+
+            {color == "red" ? <img src={apples} /> : <img src={watermelon} />}
+
             <div style={{
-                margin:"10px",
-                backgroundColor: "white",
-                border: "2px solid black",
+                margin: "10px",
+                backgroundColor: color,
+                border: color == "red" ? "5px solid black" : "5px dotted black",
                 height: "50px",
                 width: "50px"
             }} />
-            <button>Red</button><button>Green</button>
+            <button onClick={redButtonClick}>Red</button>
+
+            <button onClick={() => {
+                setColor("green");
+            }}>Green</button>
         </>
     )
 }
